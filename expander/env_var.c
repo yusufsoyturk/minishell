b77b2d5209200bb35ffd54f8e358c96ec45c20ee
Yusuf Soytürk <164682431+yusufsoyturk@users.noreply.github.com>
2025-06-05 17:10:32 +0000

    expander with quotes

--- expander/env_var.c
+++ expander/env_var.c
@@ -27,39 +27,135 @@ char *expand_chance_env(t_env *env_list, t_token *token, int *i)
 	return (new_val);
 }
 
-void	expand_env_var(t_env *env_list, t_token *token)
+char	*expand_env_var(t_env *env_list, t_token *token, int *i)
 {
-	int	i;
 	char *new_val;
 	char *new_val2;
 	// char *tmp;
 
-	i = 0;
 	new_val = NULL;
 	new_val2 = NULL;
 	// tmp = NULL;
-	while (token->value[i])
+	while (token->value[*i])
 	{
-		if (token->value[i] == '$')
+		if (token->value[*i] == '$')
 		{
 			if (!new_val)
 			{
-				new_val = ft_strdup(expand_chance_env(env_list, token, &i));
+				new_val = ft_strdup(expand_chance_env(env_list, token, i));
 			}
 			else if (new_val)
 			{
 				if (new_val2)
 					free(new_val2);
-				new_val2 = ft_strdup(expand_chance_env(env_list, token, &i));
+				new_val2 = ft_strdup(expand_chance_env(env_list, token, i));
 				new_val = ft_strjoin(new_val, new_val2);
 			}
 		}
 		else
-			i++;
+			break;
 	}
-	free(token->value);
-	token->value = new_val;
+	return (new_val);
 	// printf("%d %s\n", i, new_val);
+}
+
+char *expand_pre(t_env *env_list, t_token *token, int *i)
+{
+	int	pre;
+	int a;
+	char *new_val;
+
+	(void)*env_list;
+	a = 0;
+	new_val = NULL;
+	pre = *i;
+	while (token->value[pre] && token->value[pre] != '$' && token->value[pre] != 39)
+		pre++;
+	new_val = malloc(sizeof(char) * pre + 1);
+	while (pre > 0)
+	{
+		new_val[a] = token->value[*i];
+		(*i)++;
+		a++;
+	}
+	new_val[a] = '\0';
+	return (new_val);
+}
+char *expand_pre_quo(t_env *env_list, t_token *token, int *i)
+{
+	int end;
+	char *new_val;
+	int a;
+
+	(void)*env_list;
+	a = 0;
+	new_val = NULL;
+	end = (*i);
+	while (token->value[end + 1] && token->value[end + 1] != 39)
+		end++;
+	end++;
+	new_val = malloc(sizeof(char) * end - (*i) + 1);
+	while ((*i) < end)
+	{
+		new_val[a] = token->value[*i];
+		a++;
+		(*i)++;
+	}
+	new_val[a] = '\0';
+	return (new_val);
+}
+
+char	*expand_with_quotes(t_env *env_list, t_token *token, int *i)
+{
+	char *new_val;
+	char *new_val2;
+
+	new_val = NULL;
+	new_val2 = NULL;
+	while (token->value[*i])
+	{
+		if (token->value[*i] != 39 && token->value[*i] != '$')
+		{
+			if (!new_val)
+				new_val = ft_strdup(expand_pre(env_list, token, i));
+			else
+			{
+				if (new_val2)
+					free(new_val2);
+				new_val2 =  ft_strdup(expand_pre(env_list, token, i));
+				new_val = ft_strjoin(new_val, new_val2);
+			}
+		}
+		else if (token->value[*i] != '$')
+		{
+			if (!new_val)
+				new_val = ft_strdup(expand_pre_quo(env_list, token, i));
+			else
+			{
+				if (new_val2)
+					free(new_val2);
+				new_val2 = ft_strdup(expand_pre_quo(env_list, token, i));
+				new_val = ft_strjoin(new_val, new_val2);
+			}
+		}
+		else if (token->value[*i] == '$')
+		{
+			if (!new_val)
+				new_val = ft_strdup(expand_env_var(env_list, token, i));
+			else
+			{
+				if (new_val2)
+					free(new_val2);
+				new_val2 = ft_strdup(expand_env_var(env_list, token, i));
+				new_val = ft_strjoin(new_val, new_val2);
+			}
+		}
+		else
+			(*i)++;
+	}
+	if (new_val2)
+		free(new_val2);
+	return (new_val);
 }
 
 int dollar_control(t_token *token)
@@ -78,18 +174,29 @@ int dollar_control(t_token *token)
 
 void	ft_expand(t_env *env_list, t_token *token)
 {
-	(void)*env_list;
+	int	i;
+	char *new_value;
+	
+	i = 0;
+	new_value = NULL;
 	while (token)
 	{
-		if (token->type == T_ENV_VAR)
+		// if (token->type == T_ENV_VAR)
+		// {
+		// 	// printf("%s\n", token->value);
+		// 	expand_env_var(env_list, token);
+		// }
+		if (token->type == T_WORD || token->type == T_ENV_VAR)
 		{
-			// printf("%s\n", token->value);
-			expand_env_var(env_list, token);
-		}
-		else if (token->type == T_WORD)
-		{
-			printf("yusuf\n");
-			// if (dollar_control(token) == 1)
+			if (dollar_control(token) == 1)
+			{
+				new_value = expand_with_quotes(env_list, token, &i);
+				if (token->value)
+				{
+					free(token->value);
+					token->value = new_value;
+				}
+			}
 			// 	expand_with_quotes(env_list, token);
 		}
 		token = token->next;
--- inc/minishell.h
+++ inc/minishell.h
@@ -66,7 +66,7 @@ void	sort_env_array(t_env **arr);
 
 
 // expander env_var
-void	expand_env_var(t_env *env_list, t_token *token);
+char	*expand_env_var(t_env *env_list, t_token *token, int *i);
 void	ft_expand(t_env *env_list, t_token *token);
 
 void	free_token(t_token *token);
