55146d77e829e08fbd513b8d68255bd38765bbe2
Yusuf Islam Soytürk <ysoyturk@k1m04s10.42istanbul.com.tr>
2025-06-25 20:49:46 +0300

    free added

---
+++ utils/free.c
@@ -0,0 +1,83 @@
+#include "../inc/minishell.h"
+
+void	free_double(t_shell *minishell)
+{
+	int	i;
+
+	i = 0;
+	while (minishell->args[i])
+	{
+		free(minishell->args[i]);
+		i++;
+	}
+	free(minishell->args);
+}
+
+void	free_struct(t_shell *minishell)
+{
+	free(minishell->line);
+	free_double(minishell);
+	free_token(minishell->token);
+	free(minishell);
+}
+
+void	free_env_list(t_env *env_list)
+{
+    t_env *tmp;
+
+    while (env_list)
+    {
+        tmp = env_list;
+        env_list = env_list->next;
+        free(tmp->key);
+        free(tmp->value);
+        free(tmp);
+    }
+}
+
+void free_redirections(t_redir *redir)
+{
+    t_redir	*tmp;
+
+    while (redir)
+    {
+        tmp = redir;
+        redir = redir->next;
+        free(tmp->target);
+        free(tmp);
+    }
+}
+
+void free_commands(t_command *cmd)
+{
+    t_command	*tmp;
+    int			i;
+
+    while (cmd)
+    {
+        tmp = cmd;
+        cmd = cmd->next;
+        if (tmp->args)
+        {
+            for (i = 0; tmp->args[i]; i++)
+                free(tmp->args[i]);
+            free(tmp->args);
+        }
+        free_redirections(tmp->redirs);
+        free(tmp);
+    }
+}
+
+void	free_env(t_env *env)
+{
+	t_env *tmp;
+
+	while (env)
+	{
+		tmp = env->next;
+		free(env->key);
+		free(env->value);
+		free(env);
+		env = tmp;
+	}
+}
