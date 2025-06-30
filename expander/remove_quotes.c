372e248801cf344894932d2ca9c9af4777acc849
Yusuf Islam Soytürk <ysoyturk@k1m03s07.42istanbul.com.tr>
2025-06-30 22:17:34 +0300

    quotes remover

--- expander/remove_quotes.c
+++ expander/remove_quotes.c
@@ -6,19 +6,19 @@
 /*   By: ysoyturk <ysoyturk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 14:55:20 by ysoyturk          #+#    #+#             */
-/*   Updated: 2025/06/22 12:59:32 by ysoyturk         ###   ########.fr       */
+/*   Updated: 2025/06/30 22:07:10 by ysoyturk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-// "ysufu isalam 'abc "
+// " ' a"
 
 void	quotes_remover(t_token *token)
 {
 	char	*new_val;
 	int		len;
-	char	q_flag;
+	int		q_flag;
 	int		i;
 	int		j;
 
@@ -26,24 +26,29 @@ void	quotes_remover(t_token *token)
 	new_val = malloc(sizeof(char) * len + 1);
 	i = 0;
 	j = 0;
+	q_flag = 0;
 	while (token->value[i])
 	{
-		if (token->value[i] == 39 || token->value[i] == 34)
+		if (token->value[i] == 34)
 		{
-			if (q_flag == 0)
-				q_flag = token->value[i];
-			else if (q_flag == token->value[i])
-				q_flag = 0;
+			i++;
+			while (token->value[i] && token->value[i] != 34)
+				new_val[j++] = token->value[i++];
+			i++;
+		}
+		else if (token->value[i] == 39)
+		{
+			i++;
+			while (token->value[i] && token->value[i] != 39)
+				new_val[j++] = token->value[i++];
+			i++;
 		}
 		else
-			new_val[j++] = token->value[i];
-		i++;
+			new_val[j++] = token->value[i++];
 	}
 	new_val[j] = '\0';
-	if (new_val)
-		free(token->value);
-	if (new_val)
-		token->value = new_val;
+	free(token->value);
+	token->value = new_val;
 }
 
 void	remove_quotes(t_token *token)
